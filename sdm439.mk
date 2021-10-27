c1c882a9ca490d2bda38b7f133c2700a862ec5bc
skittels15 <skittels1510@mail.ru>
2021-10-27 14:49:21 +0300

    sdm439-common: Gaming mode and some fixes

--- sdm439.mk
+++ sdm439.mk
@@ -170,6 +170,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
 
+#Gaming Mode
+PRODUCT_PACKAGES += \
+	init.performance_level.rc
+	
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
